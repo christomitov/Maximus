d7666d140fabb52c3ab51995665622016c914f33
Christo Mitov <christo.mitov@gmail.com>
2014-12-25 04:01:05 -0500

    fixed whitespace issue with container not taking 100% height

--- client/app/invoices/invoices.controller.js
+++ client/app/invoices/invoices.controller.js
@@ -2,6 +2,7 @@
 
 angular.module('maximalistApp')
   .controller('InvoicesCtrl', function($scope, Modal, Item) {
+    
     $scope.skuNumbers = [{
       sku: '11111',
       name: 'something'
@@ -16,7 +17,7 @@ angular.module('maximalistApp')
       // Fetch the item from the database via SKU
       var item = Item.getItem(selected.title);
       console.log(item);
-      
+
       // Load the modal with this item
       var saveItemModal = Modal.confirm.save(saveItemToInvoice, null, item);
       // Render the modal
